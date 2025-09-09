cb02a457a1a25a63e8d17a60e9333d2aaac967f9
yaninard <yaninard267@gmail.com>
2025-09-08 23:00:06 -0300

    aumente cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express= require("express");
-
+const cors = require("cors");
 const app= express();
 const dbconnect = require("./config/db");
 const productoRoutes = require("./routes/producto");
 
 
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 
 app.use(express.json());
 
--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dbconnect = async () => {
     try {
         await mongoose.connect("mongodb://localhost:27017/producto");
+
                 console.log("Conexión a la base de datos exitosa");
     } catch (error) {
         console.error("Error al conectar a la base de datos:", error);

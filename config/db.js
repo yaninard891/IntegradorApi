b0e0eaa1dde984d89070025e5661116c9721f291
yaninard <yaninard267@gmail.com>
2025-09-12 20:48:51 -0300

    listo para subir nube

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbconnect = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/producto");
+        await mongoose.connect(process.env.MONGO_URI);
 
                 console.log("Conexión a la base de datos exitosa");
     } catch (error) {
---
+++ server.js
@@ -0,0 +1,22 @@
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dbconnect = require("./config/db");
+
+const productoRoutes = require("./routes/producto");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api", productoRoutes);
+
+dbconnect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+});

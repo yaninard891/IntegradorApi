80ae8893eaad3667997014391323056321a57f03
yaninard <yaninard267@gmail.com>
2025-09-12 21:26:40 -0300

    cambio de puerto para render

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const dbconnect = require("./config/db");
 const productoRoutes = require("./routes/producto");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());

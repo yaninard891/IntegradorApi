0daad354dc1a00bc55dc17d4aec8a09575313ca2
yaninard <yaninard267@gmail.com>
2025-09-12 21:40:04 -0300

    modifique server

--- server.js
+++ server.js
@@ -3,24 +3,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const dbconnect = require("./config/db");
-
 const productoRoutes = require("./routes/producto");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
-
 app.use(cors());
 app.use(express.json());
-
-
 app.use("/api", productoRoutes);
 
 dbconnect().then(() => {
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
   });
+}).catch((error) => {
+  console.error("Error al conectar a la base de datos:", error);
+  process.exit(1);
 });
